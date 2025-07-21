55aa4828a2a15f33a0886fb97f658ab7bb490d1d
Janus <Janus.Haung@hualiteq.com>
2025-07-21 17:44:55 +0800

    Initial Commit

---
+++ SentWalks/SentWalksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentWalksApp.swift
+//  SentWalks
+//
+//  Created by Hualiteq International on 2025/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SentWalksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
